82c2a5a8e270091b8ae0ddbed21e9d75eec878cf
Jim Reed <cjoackin@gmail.com>
2021-04-03 11:05:04 -0300

    Added rinkeby config to truffle config file

--- truffle.js
+++ truffle.js
@@ -1,3 +1,9 @@
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const infuraKey = "fj4jll3k.....";
+//
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 const path = require('path');
 
 module.exports = {
@@ -17,6 +23,13 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     }
+    rinkeby: {
+      // project test-1
+     provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/3515c91786994b689bf3c06943f0b22c`),
+     network_id: 4,       // Ropsten's id
+     gas: 4500000,        // Ropsten has a lower block limit than mainnet
+     gasPrice: 10000000000,
+     },
   },
   contracts_build_directory: path.join(__dirname, "client3/src/contracts"),
   //networks: {
